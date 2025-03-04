e9457198722968ed699b85fd4853d85167a13493
Raissa Yrina <raissayrina@gmail.com>
2025-03-04 19:25:11 -0300

    Subindo arquivo 02

---
+++ ex_02.py
@@ -0,0 +1,7 @@
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+
+soma = n1 + n2
+print(f"A soma entre o {n1} e {n2} é {soma}")                   
